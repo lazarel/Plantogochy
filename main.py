14c1ba0adbf810d41eea5225f92b1d08380a59fa
Anastasia Janowsky <ianovskaia.aa@phystech.edu>
2019-05-23 14:58:57 +0300

    remove set_window() from main.py to game.py

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
-from game import start_game
+from game import start_game, okToPressReturn
 import tkinter as tk
 from frame import MainFrame
-
-def set_window(root):
-    root.title('Plantagotchi')
-    root.geometry('310x310+200+200')
-    root.resizable(False, False)
-
-okToPressReturn = True
 
 if __name__ == '__main__':
     game = start_game(okToPressReturn)
 
-root = tk.Tk()
-set_window(root)
-frame = MainFrame(root, game)
-
+#frame = MainFrame(root, game)
