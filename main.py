47a8718e24ddfb19ebadcd1ab62121a0643e1669
Anastasia Janowsky <ianovskaia.aa@phystech.edu>
2019-05-26 11:39:43 +0300

    edit main.py

--- main.py
+++ main.py
@@ -1,24 +1,6 @@
+from game import FullScreenApp, start_game
 
-#from game import start_game #, okToPressReturn
-import tkinter as tk
-from frame import *
-from frames_switching import *
-
-
-# def set_window(root):
-#     root.title('Plantagotchi')
-#     root.geometry('310x310+200+200')
-#     root.resizable(False, False)
 
 if __name__ == '__main__':
-    app = Pages()
+    app = FullScreenApp(parent=start_game(True))
     app.mainloop()
-    #game = start_game(True)
-
-#<<<<<<< HEAD
-# root = tk.Tk()
-# set_window(root)
-# frame = MainFrame(root, game)
-
-#frame = MainFrame(root, game)
-
